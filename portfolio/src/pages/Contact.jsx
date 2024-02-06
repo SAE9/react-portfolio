2437c914a0521cf800c49af207e9b7a69c6e94d9
cat coder <s.emama@hotmail.com>
2024-02-06 17:20:20 +0000

    added html cssto contact and added styles

--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import Form from '../components/Form';
+import '../css/contact.css';
 
-const  Contact = () => {
-    return <Form />
-  };
+const Contact = () => {
+  return (
+    <div className="container">
+      <h2>Contact Me</h2>
+      <p>
+        Feel free to reach out to me through any of the following methods:
+      </p>
+      <ul>
+        <li>Email: s.emama@hotmail.com</li>
+        <li>GitHub: <a href="https://github.com/SAE9">github.com/yourusername</a></li>
+        <li>LinkedIn: <a href="https://www.linkedin.com/in/sumayyah-e-9ab4bb192/">linkedin.com/in/yourprofile</a></li>
+        
+      </ul>
+      <hr />
+      <h3>Send me a message:</h3>
+      <Form />
+    </div>
+  );
+};
 
 export default Contact;
