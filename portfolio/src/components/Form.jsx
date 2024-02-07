5298d25b769bdfed382407e061a9a22b727c3721
cat coder <s.emama@hotmail.com>
2024-02-07 22:55:41 +0000

    edited and add to readme file

--- portfolio/src/components/Form.jsx
+++ portfolio/src/components/Form.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import '../css/contact.css';
 
 function Form() {
-  // Setting initial state to an object
+ 
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -10,10 +10,9 @@ function Form() {
   });
 
   const handleInputChange = (event) => {
-    // Getting the value and name of the input which triggered the change
+    
     const { name, value } = event.target;
 
-    // Updating the input's state
     setFormData({
       ...formData,
       [name]: value,
@@ -21,16 +20,16 @@ function Form() {
   };
 
   const handleFormSubmit = (event) => {
-    // Preventing the default behavior of the form submit 
+   
     event.preventDefault();
 
-    // Simulate sending a message
+   
     alert(`Message sent:
       Name: ${formData.name}
       Email: ${formData.email}
       Comment: ${formData.comment}`);
 
-    // Clear form fields after submitting
+    
     setFormData({
       name: '',
       email: '',
