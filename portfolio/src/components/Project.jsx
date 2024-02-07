178f17d1405b5335161fbeb033a1b40599c245c9
cat coder <s.emama@hotmail.com>
2024-02-07 18:12:37 +0000

    Added images and links data to project json file

--- portfolio/src/components/Project.jsx
+++ portfolio/src/components/Project.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-function Project(props) {
-    // Body
-    return <div className="project-card">
-        <h2>{props.title}</h2>
-        <p>{props.description}</p>
-        <div className="links">
-            <a href={deployedLink} target="_blank" rel='noopener noreferrer'>Deployed Link</a>
-            <a href={github} target="_blank" rel='noopener noreferrer'>Github Link</a>
-        </div>
-        <img src={image} alt={title} className="image" />
 
+const Project = ({ title, image, description, deployedLink, githubLink }) => {
+  return (
+    <div className="project-card">
+      <h2 className="card-title">{title}</h2>
+      <img src={image} alt={title} className="project-image" />
+      <p>{description}</p>
+      <div className="project-links">
+        <a href={deployedLink} target="_blank" rel="noopener noreferrer">Deployed Version</a>
+        <a href={githubLink} target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+      </div>
+      
     </div>
-}
+  );
+};
 
 export default Project;
