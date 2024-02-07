030f19d0c09fa9e8b0cdbf795fc8c470d67c4f3c
cat coder <s.emama@hotmail.com>
2024-02-07 17:26:13 +0000

    added more props like image,github,deployed link,renders to project image,title,description etc

--- portfolio/src/components/Project.jsx
+++ portfolio/src/components/Project.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 function Project(props) {
     // Body
-    return <div>
+    return <div className="project-card">
         <h2>{props.title}</h2>
         <p>{props.description}</p>
+        <div className="links">
+            <a href={deployedLink} target="_blank" rel='noopener noreferrer'>Deployed Link</a>
+            <a href={github} target="_blank" rel='noopener noreferrer'>Github Link</a>
+        </div>
+        <img src={image} alt={title} className="image" />
+
     </div>
 }
 
